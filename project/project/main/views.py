35e6a72770b8e91573aff5a201b765fbff82dec0
FattyBastard <mr.nikitausersan@mail.ru>
2021-07-01 18:22:45 +0300

    Добавление View, Models, написание html

---
+++ project/project/main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Task
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
